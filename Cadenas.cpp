c6192a53799804ae8b6d4466e523b1495be31253
vitaliy-pg <aljndrvt@myuax.com>
2023-12-16 14:05:59 +0100

    imprime los resultados

--- Cadenas.cpp
+++ Cadenas.cpp
@@ -14,8 +14,11 @@ int main() {
     int resultado1 = strcmp(cadena1, cadena2);
     int resultado2 = strcmp(cadena1, cadena3);
 
-    std::cout << "Comparacion entre cadena1 y cadena2: " << resultado1 << std::endl;
-    std::cout << "Comparacion entre cadena1 y cadena3: " << resultado2 << std::endl;
+    std::cout << "Longitud de cadena1: " << longitud1 << std::endl;
+    std::cout << "Longitud de cadena2: " << longitud2 << std::endl;
+    std::cout << "Longitud de cadena3: " << longitud3 << std::endl;
+    std::cout << "Comparación entre cadena1 y cadena2: " << resultado1 << std::endl;
+    std::cout << "Comparación entre cadena1 y cadena3: " << resultado2 << std::endl;
 
     return 0;
 
