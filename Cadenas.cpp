f74d4733431f06596dcaeb63a276f4b58c527044
vitaliy-pg <aljndrvt@myuax.com>
2023-12-16 14:01:53 +0100

    compara las cadenas

--- Cadenas.cpp
+++ Cadenas.cpp
@@ -7,4 +7,7 @@ int main() {
     const char* cadena2 = "Hola";
     const char* cadena3 = "Mundo";
 
+    int resultado1 = strcmp(cadena1, cadena2);
+    int resultado2 = strcmp(cadena1, cadena3)
+
 }
