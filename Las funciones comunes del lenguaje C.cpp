dd4d83205c348c1920b276497db5e80aa06ca14f
vitaliy-pg <aljndrvt@myuax.com>
2023-12-16 13:48:16 +0100

    ejemplo para un numero positivo

--- Las funciones comunes del lenguaje C.cpp
+++ Las funciones comunes del lenguaje C.cpp
@@ -1,3 +1,7 @@
 //programa basasdo en calculo del valor absoluto de un numero
 #include <iostream>
 int main() {
+    int numeroPositivo = 10;
+    int valorAbsPositivo = abs(numeroPositivo);
+    std::cout << "Valor absoluto de " << numeroPositivo << ": " << valorAbsPositivo << std::endl;
+}
