17b73d0231641172c68dce98d8a6c0258e7bc8e8
vitaliy-pg <aljndrvt@myuax.com>
2023-12-16 14:03:07 +0100

    quito acentos

--- Cadenas.cpp
+++ Cadenas.cpp
@@ -10,8 +10,8 @@ int main() {
     int resultado1 = strcmp(cadena1, cadena2);
     int resultado2 = strcmp(cadena1, cadena3);
 
-    std::cout << "Comparación entre cadena1 y cadena2: " << resultado1 << std::endl;
-    std::cout << "Comparación entre cadena1 y cadena3: " << resultado2 << std::endl;
+    std::cout << "Comparacion entre cadena1 y cadena2: " << resultado1 << std::endl;
+    std::cout << "Comparacion entre cadena1 y cadena3: " << resultado2 << std::endl;
 
     return 0;
 
